6e7927c80be4d0f9018721fbe8ac9fbd647e8682
csuftitan <csuftitan@MacBook-Air-44.lan>
2024-03-26 02:18:16 -0700

    bug fix4 in prod server

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -74,7 +74,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
   console.log(formData);
-  console.log(typeof formData.amount);
+  console.log(typeof formData.get('amount'));
 }
 export async function updateInvoice(id: string, formData: FormData) {
   const { customerId, amount, status } = UpdateInvoice.parse({
